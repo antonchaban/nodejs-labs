e223308027e9bf6da8b3aa5b46caa1a93296b0a1
Maryna Tsuk <73252923+marynatsuk@users.noreply.github.com>
2023-03-01 15:11:08 +0200

    Update josephus.js

--- lab1/josephus.js
+++ lab1/josephus.js
@@ -2,12 +2,29 @@
 // Permutation - https://www.codewars.com/kata/5550d638a99ddb113e0000a2/train/javascript
 // Survivor - https://www.codewars.com/kata/555624b601231dc7a400017a/train/javascript
 
+//united function
+function josephus(n, k) {
+    //create array of people
+    let alive = new Array(n);
+    for (let i = 0; i < n; i++)
+        alive[i] = i+1;
+    let current = 0, dead = [];
+    while (alive.length > 1) { // while there are still people
+        current = (current + k - 1) % alive.length; //choose position to delete
+        dead.push(alive[current]);
+        alive.splice(current, 1); //delete and move array
+    }
+    console.log('Josephus permutation of ' + n + ' people with ' + k + ' interval - ' + dead);
+    console.log('Josephus Survivor of ' + n + ' people with ' + k + ' interval - ' + alive.pop());
+}
+
 function create_fill_array(n) {
     const array = new Array(n);
     for (let i = 0; i < n; i++)
         array[i] = i+1;
     return array
 }
+// https://www.codewars.com/kata/555624b601231dc7a400017a/train/javascript
 function josephusSurvivor(n,k){
     //create array of people
     let alive = create_fill_array(n);
@@ -21,7 +38,7 @@ function josephusSurvivor(n,k){
     console.log('Josephus Survivor of ' + n + ' people with ' + k + ' interval - ' + alive.pop());
     return alive.pop();
 }
-
+//https://www.codewars.com/kata/5550d638a99ddb113e0000a2/train/javascript
 function josephusPermutation(n, k) {
     let alive = create_fill_array(n);
     let current = 0,
@@ -41,4 +58,7 @@ josephusPermutation(10, 1) //exp. result: [1,2,3,4,5,6,7,8,9,10]
 josephusSurvivor(10, 1) //exp.result: 10
 josephusPermutation(7, 3) //exp. result: [3, 6, 2, 7, 5, 1, 4]
 josephusSurvivor(7, 3) //exp.result: 4
+
+josephus(10, 1);
+josephus(7, 3);
 
